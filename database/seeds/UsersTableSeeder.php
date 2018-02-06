f2abdae09ecd18680b512876549892f372f568bd
Gustavo Colombelli Alessio <gustavocolombelli@gmail.com>
2018-02-06 18:03:18 -0200

    Inserção AdminLTE + Login

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+        	'name' 		=> 'Gustavo Colombelli Alessio',
+        	'email' 	=> 'gustavocolombelli@gmail.com',
+        	'password'  => bcrypt('123456')
+        ]);
+
+        User::create([
+        	'name' 		=> 'Thiago Valente',
+        	'email' 	=> 'thiago_valente@outlook.com',
+        	'password'  => bcrypt('123456')
+        ]);
+    }
+}
